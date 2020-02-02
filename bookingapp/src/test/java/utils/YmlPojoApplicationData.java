debcfa9a55a2257529e57be9ead780f509b29cf5
S787971 <nitish.sharma1@dnata.com>
2020-02-02 15:45:02 +0400

    updated .gitignore

--- bookingapp/src/test/java/utils/YmlPojoApplicationData.java
+++ bookingapp/src/test/java/utils/YmlPojoApplicationData.java
@@ -10,5 +10,8 @@ public class YmlPojoApplicationData {
         return this.appData;
     }
 
+    public void setAppdata(HashMap<String, String> Appdata) {
+        this.appData = Appdata;
+    }
 
 }
